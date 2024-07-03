1e7aae39dce8d5d13093f51db4bded8801532846
Bruno <bvicente94@hotmail.com>
2024-07-03 19:53:59 +0100

    First commit

---
+++ Helpers/FilesUtilities.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderSynchronization.Helpers
+{
+    internal class FilesUtilities
+    {
+        public static bool FilesAreEqual_Hash(string file1, string file2)
+        {
+            return FilesAreEqual_Hash(new FileInfo(file1), new FileInfo(file2));
+        }
+
+        public static bool FilesAreEqual_Hash(FileInfo file1, FileInfo file2)
+        {
+            byte[] file1Hash = SHA256.Create().ComputeHash(file1.OpenRead());
+            byte[] file2Hash = SHA256.Create().ComputeHash(file2.OpenRead());
+
+            for(int i = 0; i < file1Hash.Length; i++)
+            {
+                if (file1Hash[i] != file2Hash[i])
+                {
+                    return false; 
+                }
+            }
+            return true;
+        }
+
+        public static void CopyDirectories(string sourceDir, string replicaDir)
+        {
+            CopyDirectories(new DirectoryInfo(sourceDir), new DirectoryInfo(replicaDir));
+        }
+
+        public static void CopyDirectories(DirectoryInfo sourceDirInfo, DirectoryInfo replicaDirInfo)
+        {
+            DirectoryInfo[] sourceSubDirInfo = sourceDirInfo.GetDirectories();
+            DirectoryInfo[] replicaSubDirInfo = replicaDirInfo.GetDirectories();
+
+            foreach (var subDirInfo in sourceSubDirInfo)
+            {
+                if (!replicaSubDirInfo.Select(x => x.Name).ToList().Contains(subDirInfo.Name))
+                {
+                    var replicaNewDirectoryInfo = Directory.CreateDirectory(Path.Join(replicaDirInfo.FullName, subDirInfo.Name));
+                    CopyDirectories(subDirInfo, replicaNewDirectoryInfo);
+                }
+            }
+            return;
+        }
+
+        public static string[] GetFiles(string directory) => Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+    }
+}
--- Worker.cs
+++ Worker.cs
@@ -1,23 +1,44 @@
+using FolderSynchronization.Helpers;
+
 namespace FolderSynchronization;
 
-public class Worker : BackgroundService
+public class Worker(ILogger<Worker> logger, IConfiguration configuration) : BackgroundService
 {
-    private readonly ILogger<Worker> _logger;
-
-    public Worker(ILogger<Worker> logger)
-    {
-        _logger = logger;
-    }
+    private readonly ILogger<Worker> _logger = logger;
+    private readonly IConfiguration _configuration = configuration;
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        string sourceDir = _configuration["SourceDirectory"]!;
+        string replicaDir = _configuration["ReplicaDirectory"]!;
         while (!stoppingToken.IsCancellationRequested)
         {
-            if (_logger.IsEnabled(LogLevel.Information))
+            _logger.LogInformation("Starting the synchronization process...");
+            _logger.LogInformation($"Source Folder: {sourceDir}");
+            _logger.LogInformation($"Replica Folder: {replicaDir}");
+
+            Task[] allTasks = [];
+
+            FilesUtilities.CopyDirectories(sourceDir, replicaDir);
+
+            string[] sourceDirFiles = FilesUtilities.GetFiles(sourceDir);
+            string[] replicaDirFiles = FilesUtilities.GetFiles(replicaDir);
+            foreach (var file in sourceDirFiles)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                allTasks.Append(Task.Run(() => File.Copy(file, Path.Join(replicaDir, Path.GetRelativePath(sourceDir, file))), stoppingToken)
+                    .ContinueWith(task =>
+                    {
+                        if (task.IsCompletedSuccessfully)
+                        {
+                            _logger.LogInformation($"Copied \"{Path.GetFileName(file)}\" from {sourceDir} to {replicaDir}.");
+                        }
+                    }, stoppingToken)
+                );
             }
-            await Task.Delay(1000, stoppingToken);
+
+            await Task.Delay(int.Parse(_configuration["SynchronizationInterval"]!), stoppingToken);
+            Task.WaitAll(allTasks, stoppingToken);
+            _logger.LogInformation("Synchronization finished.");
         }
     }
 }
